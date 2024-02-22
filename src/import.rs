9e4f5b5c534e1c56b41e7abd5e06a992aab93c02
Timothy Spring <lordfoom@gmail.com>
2024-02-22 18:41:51 +0200

    Continue with the implementation of importing file

--- src/import.rs
+++ src/import.rs
@@ -1,7 +1,17 @@
+use std::fs::File;
+
 use anyhow::Result;
 ///Import a file into the flashcards using the ReadEra exported format
 ///Top line will be used as the title for flashcards, prefixed with a monotonically increasing
 ///number
 pub fn import_read_era_quotes(fp: &str) -> Result<()> {
+    let file_contents = std::fs::read_to_string(fp)?;
+    //now we parse the file contents
+    //first line: title
+    //second line: author
+    //each entry is separated by
+    //*****
+    for flash_card in file_contents.split("*****") {}
+
     Ok(())
 }
