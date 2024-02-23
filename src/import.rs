45b2ce2c841f9bcc81996e8b30c1bf52b0a63d84
Timothy Spring <lordfoom@gmail.com>
2024-02-23 16:34:38 +0200

    Parse note files into approprate sections
    Gonna be sticking know how in my brain soon!

--- src/import.rs
+++ src/import.rs
@@ -1,17 +1,43 @@
 use std::fs::File;
 
 use anyhow::Result;
+use rusqlite::Connection;
 ///Import a file into the flashcards using the ReadEra exported format
 ///Top line will be used as the title for flashcards, prefixed with a monotonically increasing
 ///number
-pub fn import_read_era_quotes(fp: &str) -> Result<()> {
+pub fn import_read_era_quotes(fp: &str, conn: &Connection) -> Result<()> {
     let file_contents = std::fs::read_to_string(fp)?;
     //now we parse the file contents
     //first line: title
     //second line: author
+    //rest is entries
+    let mut title = String::new();
+    let mut author = String::new();
+
     //each entry is separated by
     //*****
-    for flash_card in file_contents.split("*****") {}
+    for (i, flash_card) in file_contents.split("*****").enumerate() {
+        let mut body = String::new();
+        if i == 0 {
+            for (j, line) in flash_card.split('\n').enumerate() {
+                //book title
+                if j == 0 {
+                    title = line.to_string();
+                } else if j == 1 {
+                    //author
+                    author = line.to_string(); //
+                } else {
+                    body.push_str(line);
+                    body.push('\n');
+                }
+            }
+        } else {
+            //not the title card
+            body = flash_card.to_string();
+        }
+    }
+    //TODO here wi stick it into the db, will need to pass in conn
+    //OOOOR do we return the collection?
 
     Ok(())
 }
