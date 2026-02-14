52137d720e7c00aa088317a24dfe0b0b73a3915c
LordFoom <lordfoom@gmail.com>
2026-02-14 21:42:04 +0200

    Pump up the colors

--- src/report.rs
+++ src/report.rs
@@ -5,9 +5,13 @@ use crate::db;
 
 pub fn print_out_report(title_report: &db::CardTitleReport) -> Result<()> {
     println!("{}", "Report on titles".red());
-    println!("{}", "=================".magenta());
+    println!("{}", "=================".yellow());
     title_report.report_lines.iter().for_each(|line| {
-        println!("{} -> {}", line.title, line.title_count);
+        println!(
+            "{} -> {}",
+            line.title.cyan(),
+            line.title_count.to_string().purple()
+        );
     });
     Ok(())
 }
